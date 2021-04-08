5ea547b9943e6efba070d5236255b13b7379594c
vl4nkzz <vietanhtm@gmail.com>
2021-04-08 19:24:24 +0700

    Khach San UDP 8/4

---
+++ src/KhachSanUDP/KhachSanUDPClient.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package KhachSanUDP;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author er1nzz
+ */
+public class KhachSanUDPClient {
+    
+    public static void menu() {
+        System.out.println("1.Tìm kiếm phòng ");
+        System.out.println("2.Hiển thị tất cả phòng");
+        System.out.println("3.Thuê phòng");
+        System.out.println("4.Thoát");
+        System.out.println("Lựa chọn: ");
+    }
+    
+    public static void main(String[] args) throws SocketException, UnknownHostException, IOException {
+        DatagramSocket client = new DatagramSocket();
+        InetAddress IP = InetAddress.getByName("localhost");
+        byte[] buffer = new byte[1024];
+        
+        Scanner sc = new Scanner(System.in);
+        int rep = 0;
+        while (rep != 4) {
+            menu();
+            rep = sc.nextInt();
+            sc.nextLine();
+            
+            buffer = new byte[1024];
+            
+            buffer = (rep+"").getBytes();
+            DatagramPacket choice = new DatagramPacket(buffer, buffer.length, IP, 10);
+            client.send(choice);
+            
+            switch (rep) {
+                case 1:
+                    System.out.println("Nhập mã số nhà cần tìm: ");
+                    String target = sc.nextLine();
+                    
+                    buffer = new byte[1024];
+                    buffer =target.getBytes();
+                    DatagramPacket targetSearch = new DatagramPacket(buffer, buffer.length, IP, 10);
+                    client.send(targetSearch);
+                    
+                    System.out.println("Mã số\tLoại\t\tĐịa chỉ\t\t\tTình trạng");
+                    
+                    buffer = new byte[1024];
+                    DatagramPacket repSearch = new DatagramPacket(buffer, buffer.length);
+                    client.receive(repSearch);
+                    System.out.println(new String(repSearch.getData(),0,repSearch.getLength()));
+                    
+                    
+                    break;
+                case 2:
+                    System.out.println("Thông tin toàn bộ các nhà");
+                    System.out.println("Mã số\tLoại\t\tĐịa chỉ\t\t\tTình trạng");
+                    buffer = new byte[1024];
+                    DatagramPacket repShowAll = new DatagramPacket(buffer, buffer.length);
+                    client.receive(repShowAll);
+                    
+                    System.out.println(new String(repShowAll.getData(),0,repShowAll.getLength()));
+                    
+                    break;
+                case 3:
+                    System.out.println("Nhập mã số nhà cần thuê");
+                   
+            }
+        }
+    }
+}
---
+++ src/KhachSanUDP/KhachSanUDPServer.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package KhachSanUDP;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author er1nzz
+ */
+public class KhachSanUDPServer {
+
+    static ArrayList<PhongKhachSan> dsn = new ArrayList<>();
+
+    public static String search(String target) {
+        int i, found = 0;
+        String output = "Không tìm thấy";
+        for (i = 0; i < dsn.size(); i++) {
+            if (dsn.get(i).getID().equalsIgnoreCase(target)) {
+                output = dsn.get(i).toString();
+                found = 1;
+            }
+        }
+        if (found == 1) {
+            return output;
+        }
+        return output;
+    }
+
+    public static String showAll() {
+        int i;
+        String output = "";
+        for (i = 0; i < dsn.size(); i++) {
+            output = output + dsn.get(i).toString() + "\n";
+        }
+        return output;
+    }
+
+    public static String rentHouse(String targetID) {
+        int i, found = 0;
+        String output = "Không tìm thấy";
+        for (i = 0; i < dsn.size(); i++) {
+            if (dsn.get(i).getID().equalsIgnoreCase(targetID)) {
+                if (dsn.get(i).getStatus().equals("Đã thuê")) {
+                    return "Phòng đã thuê";
+                }
+                dsn.get(i).setStatus("Đã thuê");
+                output = "Bạn đã thuê phòng có mã số: " + dsn.get(i).getID();
+                found = 1;
+            }
+        }
+        if (found == 1) {
+            return output;
+        }
+        return output;
+    }
+
+    public static void main(String[] args) throws SocketException, FileNotFoundException, IOException {
+        DatagramSocket server = new DatagramSocket(10);
+        byte[] buffer = new byte[1024];
+
+        FileReader fr = new FileReader("KhachSan.txt");
+        BufferedReader br = new BufferedReader(fr);
+
+        String current;
+        while ((current = br.readLine()) != null) {
+
+            PhongKhachSan p = new PhongKhachSan();
+            String[] result = current.split("\\$");
+            p.setID(result[0]);
+            p.setType(result[1]);
+            p.setAddress(result[2]);
+            p.setStatus(result[3]);
+
+            System.out.println(p.toString());
+            dsn.add(p);
+
+        }
+        fr.close();
+        br.close();
+        System.out.println("Read list ok");
+
+        while (true) {
+
+            buffer = new byte[1024];
+            DatagramPacket choice = new DatagramPacket(buffer, buffer.length);
+            server.receive(choice);
+            String rep = new String(choice.getData(), 0, choice.getLength());
+            switch (rep) {
+                case "1":
+                    buffer = new byte[1024];
+                    DatagramPacket targetPacket = new DatagramPacket(buffer, buffer.length);
+                    server.receive(targetPacket);
+
+                    buffer = new byte[1024];
+                    buffer = search(new String(targetPacket.getData(), 0, targetPacket.getLength())).getBytes();
+                    DatagramPacket repSearch = new DatagramPacket(buffer, buffer.length, choice.getAddress(), choice.getPort());
+                    server.send(repSearch);
+                    
+                    break;
+                case "2":
+                    buffer = new byte[1024];
+                    buffer = showAll().getBytes();
+                    DatagramPacket repShowAll = new DatagramPacket(buffer, buffer.length, choice.getAddress(), choice.getPort());
+                    server.send(repShowAll);
+            }
+        }
+    }
+
+}
