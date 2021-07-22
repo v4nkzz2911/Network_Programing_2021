42684e555e3e806e89771e92c9361b4bb83c27c6
v4nkzz2911 <vietanhtm@gmail.com>
2021-07-22 23:12:38 +0700

    TCP 22/7 Bai 1

--- src/TrainTCP22_7/Bai1/TrainTCP22_7Bai1Client.java
+++ src/TrainTCP22_7/Bai1/TrainTCP22_7Bai1Client.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
-/**
+/** 
  *
  * @author er1nzz
  */
--- src/TrainTCP22_7/Bai1/TrainTCP22_7Bai1Server.java
+++ src/TrainTCP22_7/Bai1/TrainTCP22_7Bai1Server.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-/*
+/* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
