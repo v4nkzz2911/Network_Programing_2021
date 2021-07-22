80395157f65d3c0febd7e0303ef36b12020f0d6e
v4nkzz2911 <vietanhtm@gmail.com>
2021-07-22 23:11:20 +0700

    TDP 22/7 Bai 1

---
+++ src/TrainTCP22_7/Bai1/TrainTCP22_7Bai1Client.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TrainTCP22_7.Bai1;
+
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author er1nzz
+ */
+public class TrainTCP22_7Bai1Client {
+    public static void main(String[] args) throws IOException {
+        Socket client = new Socket("127.0.0.1", 10);
+        
+        while (true) {            
+            DataInputStream dis = new DataInputStream(client.getInputStream());
+            DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+            
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Nhập vào số nguyên dương: ");
+            
+            dos.writeUTF(sc.nextLine());
+            
+            System.out.println("Giai thừa là: "+dis.readUTF());
+        }
+    }
+}
---
+++ src/TrainTCP22_7/Bai1/TrainTCP22_7Bai1Server.java
@@ -0,0 +1,46 @@
+package TrainTCP22_7.Bai1;
+
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author er1nzz
+ */
+public class TrainTCP22_7Bai1Server {
+    public static String giaithua(int a){
+        String output;
+        int i,s=1;
+        for (i=1; i <= a ;i++){
+            s=s*i;
+        }
+        output=s+"";
+        return output;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(10);
+        Socket client = server.accept();
+        
+        while (true) {            
+            DataInputStream dis = new DataInputStream(client.getInputStream());
+            DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+            
+            
+            
+            dos.writeUTF(giaithua(Integer.parseInt(dis.readUTF())));
+            
+        }
+    }
+    
+}
